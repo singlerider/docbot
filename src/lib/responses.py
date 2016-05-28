34d146d232f104ed0c2a6fb063612252e9b80f4b
shane <haroboy876@gmail.com>
2016-05-27 21:48:37 -0700

    add responses

--- src/lib/responses.py
+++ src/lib/responses.py
@@ -1,12 +1,16 @@
 sub = {
     "first": [
         "{username} just subscribed for the first time. Pick up your Blazer in the locker room (It's the black on black on slate black one).",
-        "We've been waiting for you, {username}."
+        "We've been waiting for you, {username}.", "{username}, welcome.",
+        "Join us in the fight against stream snipers {username}",
+        "Don't leave, {username}. We'll know.",
+        "Before you ask, {username}, these aren't sunlgasses.",
+        "We don't have time for giggle games, {username}."
     ],
     "low": [
         "{username} has just resubscribed for {months} years straight! Your loyalty is admirable",
         "For {months} years, {username} has been a red bow tie-wearing high-level gamer.",
-        
+
     ],
     "high": [
 
